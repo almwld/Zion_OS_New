7c30b28bdae0592d257d04f85a503d772017651c
almwld <152663389+almwld@users.noreply.github.com>
2026-09-08 02:43:02 +0300

    security: remove obsolete offensive cosmic dashboard

--- lib/features/dashboard/cosmic_dashboard.dart
+++
@@ -1,6 +0,0 @@
-import "dart:io";
-import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
-import '../../main.dart';
-import '../../core/demon_si.dart';
-import '../../core/sage_si.dart';
