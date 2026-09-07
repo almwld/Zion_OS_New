9e6c76bee55fb0282b3f6649d82cce3f6f0d84c8
almwld <152663389+almwld@users.noreply.github.com>
2026-09-07 08:58:36 +0300

    fix(security): use cryptographically secure RSA RNG seed

--- lib/security/crypto/crypto_suite.dart
+++ lib/security/crypto/crypto_suite.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math' as math;
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart' as hash;
@@ -14,13 +15,7 @@ class CryptoSuite {
     final key = await _aes.newSecretKey();
     final nonce = _aes.newNonce();
     final box = await _aes.encrypt(utf8.encode(plaintext), secretKey: key, nonce: nonce, aad: aad ?? const []);
-    return {
-      'algorithm': 'AES-256-GCM',
-      'key': base64Encode(await key.extractBytes()),
-      'nonce': base64Encode(box.nonce),
-      'ciphertext': base64Encode(box.cipherText),
-      'mac': base64Encode(box.mac.bytes),
-    };
+    return {'algorithm': 'AES-256-GCM', 'key': base64Encode(await key.extractBytes()), 'nonce': base64Encode(box.nonce), 'ciphertext': base64Encode(box.cipherText), 'mac': base64Encode(box.mac.bytes)};
   }
 
   Future<String> aes256Decrypt(Map<String, String> payload, {List<int>? aad}) async {
@@ -31,9 +26,7 @@ class CryptoSuite {
 
   String sha512(List<int> data) => hash.sha512.convert(data).toString();
   String sha512Text(String text) => sha512(utf8.encode(text));
-
   AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRsa({int bitLength = 2048}) => _rsa.generate(bitLength: bitLength);
-
   ECDSASigner generateEcdsaSigner() => ECDSASigner(_ecdsa);
 }
 
@@ -48,7 +41,7 @@ class ECDSASigner {
 class RsaKeyHelper {
   AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generate({int bitLength = 2048}) {
     final secureRandom = FortunaRandom();
-    final seed = Uint8List.fromList(List<int>.generate(32, (i) => DateTime.now().microsecondsSinceEpoch.hashCode + i));
+    final seed = Uint8List.fromList(List<int>.generate(32, (_) => math.Random.secure().nextInt(256)));
     secureRandom.seed(KeyParameter(seed));
     final params = RSAKeyGeneratorParameters(BigInt.from(65537), bitLength, 64);
     final generator = RSAKeyGenerator()..init(ParametersWithRandom(params, secureRandom));
