52d782aaa7b483e5e522d1fec999efad330e44e2
almwld <152663389+almwld@users.noreply.github.com>
2026-09-07 06:48:49 +0300

    test: provide security core to application entry point

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,10 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:project_zion/main.dart';
+import 'package:project_zion/security/core/security_core.dart';
 
 void main() {
   test('ZionOSApp is the active application entry point', () {
-    expect(const ZionOSApp(), isA<Widget>());
+    expect(const ZionOSApp(securityCore: SecurityCore()), isA<Widget>());
   });
 }
